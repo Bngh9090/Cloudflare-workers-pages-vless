d9476be3101e0ce70148e39839e0b2c0777ccff0
Bngh9090 <159952651+Bngh9090@users.noreply.github.com>
2024-12-11 01:11:31 +0330

    _worker.js
    Qqq

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = 'XXXX';
+let userID = '9ba59bc9-5e61-45ab-a80a-71028437c568';
 
 const proxyIPs = [
     "8.222.128.131",
